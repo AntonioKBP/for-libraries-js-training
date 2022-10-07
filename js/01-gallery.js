0c835869bc6c30076a80549e808810b44ef68ccd
Anton Kovsh <kovsh_anton@ukr.net>
2022-10-07 18:41:34 +0300

    lightBox done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,18 +1,2 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
-console.log(galleryItems);
-let gallery = new SimpleLightbox(".gallery a");
-gallery.on("show.simplelightbox", function () {
-  // do something…
-});
-
-gallery.on("error.simplelightbox", function (e) {
-  console.log(e); // some usefull information
-});
-
-// with jQuery nearly the same
-let gallery = $(".gallery a").simpleLightbox();
-gallery.on("show.simplelightbox", function () {
-  // do something…
-});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,15 +1,13 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
-
 const galleryMainDiv = document.querySelector(".gallery");
 // console.log(galleryMainDiv);
 const galleryMarkup = createGalleryMarkup(galleryItems);
-console.log(galleryMarkup);
-
+// console.log(galleryMarkup);
 galleryMainDiv.insertAdjacentHTML("beforeend", galleryMarkup);
-// galleryMainDiv.addEventListener("click", onPictureClick);
+galleryMainDiv.addEventListener("click", onPictureClick);
+
 function createGalleryMarkup(gallery) {
   return gallery
     .map(({ preview, original, description }) => {
@@ -19,3 +17,39 @@ function createGalleryMarkup(gallery) {
     })
     .join("");
 }
+
+function onPictureClick(event) {
+  event.preventDefault();
+  // console.log(event.target);
+  if (event.target.tagName !== "IMG") return;
+
+  // console.log(event.target);
+  //   const instance = basicLightbox.create(
+  //     `<img src=${event.target.dataset.source}>`
+  //   );
+
+  //   instance.show();
+  //   window.addEventListener("keydown", onPress);
+  //   function onPress(event) {
+  //     // console.log(event);
+  //     if (event.code === "Escape") {
+  //       instance.close();
+  //     }
+  //   }
+}
+var lightbox = new SimpleLightbox(".gallery__item", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+// console.log(lightbox.closeOnOverlayClick);
+
+// let gallery = new SimpleLightbox(".gallery__item");
+// gallery.on("show.simplelightbox", function () {
+//   // Do something…
+// });
+// {/* <script>
+//     var lightbox = $('.gallery .col-md-6 .gallery-img a').simpleLightbox({
+//         captionPosition: top
+//     });
+// </script> */}
