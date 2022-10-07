a7e2bca669e78037014cb3000efb0f66740b375d
Anton Kovsh <kovsh_anton@ukr.net>
2022-10-07 16:37:01 +0300

    upd

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryMainDiv = document.querySelector(".gallery");
+// console.log(galleryMainDiv);
+const galleryMarkup = createGalleryMarkup(galleryItems);
+console.log(galleryMarkup);
+
+galleryMainDiv.insertAdjacentHTML("beforeend", galleryMarkup);
+// galleryMainDiv.addEventListener("click", onPictureClick);
+function createGalleryMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+<img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
